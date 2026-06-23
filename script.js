6f702183795c1af330b0ee2b942e5f6fbefbbb2f
Kordowskitransport <kajgrz1e@gmail.com>
2026-06-23 07:43:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function zaloguj() {
 
+let login = document.getElementById("login").value;
+let haslo = document.getElementById("haslo").value;
+
+if(login === "admin" && haslo === "1234"){
+    window.location.href = "index.html";
+}
+else{
+    document.getElementById("blad").innerHTML =
+    "Nieprawidłowy login lub hasło";
+}
+
+}
